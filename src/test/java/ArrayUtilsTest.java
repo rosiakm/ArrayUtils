5a3df8b18ba06c36d859e975c1276c7fee2007b4
Mateusz Rosiak <mateusz.rosiak89@gmail.com>
2025-10-22 12:25:22 +0200

    Added tests in ArrayUtilsTest.java Change @Before/After All to @Before/After Each in BaseTest.java Added final Strings in BaseTest.java and in ArrayUtils.java

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -1,3 +1,125 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
 public class ArrayUtilsTest extends BaseTest{
+
+    record ArrayTestCase(int[] input, Integer expectedMax, Integer expectedMin, Double expectedAvg){
+        @Override
+        public String toString(){
+            return "input=" + Arrays.toString(input) +
+                    ", expectedMax=" + expectedMax +
+                    ", expectedMin=" + expectedMin +
+                    ", expectedAvg=" + expectedAvg;
+        }
+    }
+
+    static Stream<ArrayTestCase> provideArrayTestCases(){
+        return Stream.of(
+                new ArrayTestCase(new int[]{-4, 0, 16, -8}, 16, -8, 1.0),
+                new ArrayTestCase(new int[]{}, null, null, null),
+                new ArrayTestCase(new int[]{6}, 6, 6, 6.0),
+                new ArrayTestCase(null, null, null, null)
+                );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("provideArrayTestCases")
+    public void maxValueInArrayTest(ArrayTestCase testCase){
+        log.info("maxValueInArrayTest is running for: {}", Arrays.toString(testCase.input()));
+        if(testCase.input() == null){
+            log.info("The array is null");
+            assertThrows(IllegalArgumentException.class, () -> arrayUtils.max(testCase.input()));
+        } else if (testCase.input().length == 0) {
+            log.info("The array is empty");
+            assertThrows(EmptyArrayException.class, () -> arrayUtils.max(testCase.input()));
+        } else {
+            assertEquals(testCase.expectedMax(), arrayUtils.max(testCase.input()),
+                    "Test failed for input: " + Arrays.toString(testCase.input()));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArrayTestCases")
+    public void minValueInArrayTest(ArrayTestCase testCase){
+        log.info("minValueInArrayTestCase is running for: {}", Arrays.toString(testCase.input()));
+        if(testCase.input() == null){
+            log.info("The array is null");
+            assertThrows(IllegalArgumentException.class, () -> arrayUtils.min(testCase.input()));
+        } else if (testCase.input().length == 0) {
+            log.info("The array is empty");
+            assertThrows(EmptyArrayException.class, () -> arrayUtils.min(testCase.input()));
+        } else {
+            assertEquals(testCase.expectedMin(), arrayUtils.min(testCase.input()),
+                    "Test failed for input: " + Arrays.toString(testCase.input()));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArrayTestCases")
+    public void avgValueInArrayTest(ArrayTestCase testCase){
+        log.info("avgValueInArrayTestCase is running for: {}", Arrays.toString(testCase.input()));
+        if(testCase.input() == null){
+            log.info("The array is null");
+            assertThrows(IllegalArgumentException.class, () -> arrayUtils.average(testCase.input()));
+        } else if (testCase.input().length == 0) {
+            log.info("The array is empty");
+            assertThrows(EmptyArrayException.class, () -> arrayUtils.average(testCase.input()));
+        } else {
+            assertEquals(testCase.expectedAvg(), arrayUtils.average(testCase.input()),
+                    "Test failed for input: " + Arrays.toString(testCase.input()));
+        }
+    }
+
+    @Test
+    public void nullArrayMessageForMaxMethodTest(){
+        log.info("The nullArrayForMaxMethodTest is running");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> arrayUtils.max(null));
+        assertEquals(NULL_ARRAY_MSG, exception.getMessage());
+    }
+
+    @Test
+    public void emptyArrayMessageForMaxMethodTest(){
+        log.info("The emptyArrayForMaxMethodTest is running");
+        Exception exception = assertThrows(EmptyArrayException.class, () -> arrayUtils.max(new int[]{}));
+        assertEquals(EMPTY_ARRAY_MSG, exception.getMessage());
+    }
+
+    @Test
+    public void nullArrayMessageForMinMethodTest(){
+        log.info("The nullArrayForMinMethodTest is running");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> arrayUtils.min(null));
+        assertEquals(NULL_ARRAY_MSG, exception.getMessage());
+    }
+
+    @Test
+    public void emptyArrayMessageForMinMethodTest(){
+        log.info("The emptyArrayForMinMethodTest is running");
+        Exception exception = assertThrows(EmptyArrayException.class, () -> arrayUtils.min(new int[]{}));
+        assertEquals(EMPTY_ARRAY_MSG, exception.getMessage());
+    }
+
+    @Test
+    public void nullArrayMessageForAverageMethodTest(){
+        log.info("The nullArrayForAverageMethodTest is running");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> arrayUtils.average(null));
+        assertEquals(NULL_ARRAY_MSG, exception.getMessage());
+    }
+
+    @Test
+    public void emptyArrayMessageForAverageMethodTest(){
+        log.info("The emptyArrayForAverageMethodTest is running");
+        Exception exception = assertThrows(EmptyArrayException.class, () -> arrayUtils.average(new int[]{}));
+        assertEquals(EMPTY_ARRAY_MSG, exception.getMessage());
+    }
 
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,20 +1,22 @@
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 @Slf4j
 public class BaseTest {
+    protected static final String NULL_ARRAY_MSG = "The array is null!";
+    protected static final String EMPTY_ARRAY_MSG = "The array is empty!";
 
     protected static ArrayUtils arrayUtils;
 
-    @BeforeAll
-    public static void setupAll(){
+    @BeforeEach
+    public void setupAll(){
         log.info("<<<< New ArrayUtils instance has been created >>>>");
         arrayUtils = new ArrayUtils();
     }
 
-    @AfterAll
-    public static void tearDown(){
+    @AfterEach
+    public void tearDown(){
         log.info("<<<< ArrayUtils instance is teared down >>>>");
         arrayUtils = null;
     }
